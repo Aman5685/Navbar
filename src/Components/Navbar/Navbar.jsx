42a4fd91cbe9075a812a43d43ec7208afc6db4a6
Aman5685 <amanyadav5685@gmail.com>
2024-10-27 22:48:51 +0530

    Done

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+// import React from 'react';
+import './Navbar.css';
+function Navbar() {
+  return (
+   <>
+   <header className="header">
+    <a href="/" className="logo">Logo</a>
+
+      <nav className="navbar">
+        <a href="/">Home</a>
+        <a href="/">About us</a>
+        <a href="/">Services</a>
+        <a href="/">Track your Shippment</a>
+        <a href="/">FQA</a>
+        <a href="/">Contact us</a>
+      </nav>
+   </header>
+   </>
+   )
+}
+
+export default Navbar
